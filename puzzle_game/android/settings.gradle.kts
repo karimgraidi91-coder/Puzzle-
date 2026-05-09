d38d43bdc06118b5a020babf25b38954567e89f0
karimgraidi91-coder <karimgraidi91@gmail.com>
2026-05-09 13:26:10 +0000

    Fixing gradle errors

--- puzzle_game/android/settings.gradle.kts
+++ puzzle_game/android/settings.gradle.kts
@@ -6,10 +6,15 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
+rootProject.name = "puzzle_game"
 
 include(":app")
